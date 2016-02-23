5d8fd0c17bc4f7d13dbb07024ebb34f9eb24e530
Hugo Chan <hugochan2013@gmail.com>
2016-02-22 20:36:03 -0500

    add cut_exercise

---
+++ Prolog codes/cut_exercise.pl
@@ -0,0 +1,24 @@
+% 1)
+% rainy(seattle).
+% rainy(rochester).
+% cold(rochester).
+% snowy(X) :- rainy(X), !, cold(X).
+
+% 2)
+% rainy(seattle) :- !.
+% rainy(rochester).
+% cold(rochester).
+% snowy(X) :- rainy(X), cold(X).
+% snowy(troy).
+
+% 3)
+% rainy(seattle).
+% rainy(rochester).
+% cold(rochester).
+% snowy(X) :- !, rainy(X), cold(X).
+
+% 4)
+rainy(seattle).
+rainy(rochester).
+cold(rochester).
+snowy(X) :- rainy(X), cold(X), !.
