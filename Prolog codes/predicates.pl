547caee19fe5dd0eb37ca1582317f5243cbc92e9
Hugo Chan <hugochan2013@gmail.com>
2016-02-22 20:02:02 -0500

    add exercise codes

---
+++ Prolog codes/predicates.pl
@@ -0,0 +1,30 @@
+member(A, [A | _]).
+member(A, [ _ | C]) :- member(A, C).
+
+append([], A, A).
+append([A | B], C, [A | D]) :- append(B, C, D).
+
+flatten([], []).
+flatten([A | B], [[A] | C]) :- flatten(B, C).
+
+% takes a list of integers and computes the sum of the integers.
+sum([], 0).
+sum([A | B], R) :- sum(B, R1), R is A + R1.
+
+% integers are arbitrarily nested.
+sum2([], 0).
+sum2(A, A) :- integer(A).
+sum2([A | B], R) :- sum2(A, R1), sum2(B, R2), R is R1 + R2.
+
+% takes a list of integers and computes another list, where all integers are shifted +10.
+plus10([], []).
+plus10([A | B], [C | D]) :- C is A + 10, plus10(B, D).
+
+% takes a list and computes the length of the list.
+len([], 0).
+len([_ | B], N) :- len(B, N1), N is N1 + 1.
+
+% takes a list and computes the number of atoms in the list.
+atoms([], 0).
+atoms(A, 1):- atom(A). % built-in predicate atom(X) yields true if X is an atom.
+atoms([A | B], R) :- atoms(A, R1), atoms(B, R2), R is R1 + R2.
