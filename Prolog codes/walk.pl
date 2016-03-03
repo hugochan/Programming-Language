5539e93157bfdc8a62549da8d7bbf48f6d32f916
Hugo Chan <hugochan2013@gmail.com>
2016-03-02 20:55:54 -0500

    edit walk

--- Prolog codes/walk.pl
+++ Prolog codes/walk.pl
@@ -1,6 +1,8 @@
 %bintree(1, bintree(2, bintree(3, null, null), bintree(4, null, null)), bintree(5, null, null)).
 
-walk(null, [null]).
-walk(Tree, T) :- parse(Tree, H, L, R), walk(L, T1), walk(R, T2), append(T1, [H], TT), append(TT, T2, T).
 
+walk(null, [null]).
+walk(Tree, [H|T]) :- parse(Tree, H, L, R), walk(L, T1), walk(R, T2), append(T1, T2, T). % pre-order
+% walk(Tree, T) :- parse(Tree, H, L, R), walk(L, T1), walk(R, T2), append(T1, [H], TT), append(TT, T2, T). % in-order
+% walk(Tree, T) :- parse(Tree, H, L, R), walk(L, T1), walk(R, T2), append(T1, T2, TT), append(TT, [H], T). % post-order
 parse(bintree(H, L, R), H, L, R).
